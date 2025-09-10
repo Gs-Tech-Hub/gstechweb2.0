20cea46553d541ff3e115fc45fce9f0fbe9346e4
tobiayinmiro23 <ayinmirotobi@gmail.com>
2025-09-10 08:35:24 +0100

    added some packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <h1 className='font-bold bg-[red]'>gs tech</h1>
     </>
   )
 }
